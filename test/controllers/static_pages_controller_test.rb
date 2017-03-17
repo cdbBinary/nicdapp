6d8e9d6b55223b9f9720d14b1da1e16de7e9667a
cdb.binary <cdb.binary@gmail.com>
2017-03-17 12:41:46 -0600

    commented about test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,5 +14,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # test "should get about" do
   # 	get static_pages_about_url
   # 	assert_response :success
-  end
+  # end
 end
