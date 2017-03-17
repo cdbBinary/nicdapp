1aa8bfd7f6f97951fcd7fb6b0dedac4c95881b92
cdb.binary <cdb.binary@gmail.com>
2017-03-17 10:20:32 -0600

    edited help and home views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'foo/bar'
+
+  get 'foo/baz'
+
   get 'static_pages/home'
   get 'static_pages/help'
   root 'application#hello'
