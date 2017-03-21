a642cd47fec3c756ab0d176a8eb65fe14e70a35f
charlie <charlie@Patricks-iMac-2.local>
2017-03-20 22:48:37 -0600

    modifed Gemfile and added helper in helpers.rb file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#Returs the ful title on a per-page basis.
+	def full_title(per-page= '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
