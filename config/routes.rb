00ddfcd9eaa249a79aff4707b1b79d725d709291
cdb.binary <cdb.binary@gmail.com>
2017-04-18 19:34:30 -0600

    cleaned up routes flie

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
