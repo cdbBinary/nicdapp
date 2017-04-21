df49e2b62b1e2e3bbc9181c5e600b47c7c51e397
cdb.binary <cdb.binary@gmail.com>
2017-04-21 19:56:38 -0400

    updated user test files

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -36,6 +36,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addrsses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.Com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "email validates should reject invalid addresses" do 
   	invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.
   													foo@bar_baz.com foo@bar+baz.com)
