16acaf4cae75bd5e51eba5254b6b47318c9bc2bf
cdb.binary <cdb.binary@gmail.com>
2017-04-21 22:12:23 -0400

    updated user test and edited syntax error for technical sophistication

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,12 +44,22 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "email validates should reject invalid addresses" do 
-  	invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.
-  													foo@bar_baz.com foo@bar+baz.com)
-  	invalid_addresses.each do |invalid_address|
-  		@user.email = invalid_address
-  		assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-  	end
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
