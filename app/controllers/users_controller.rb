4a7a0b903581626c5613c9c6a747391c264eb341
cdb.binary <cdb.binary@gmail.com>
2017-04-22 07:40:41 -0400

    updated codebase for sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,16 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user - User.new(params[:user]) # Not the final implementation!
+  	@user = User.new(user_params) # Not the final implementation!
   	if @user.save
   		# Handle a successful save.
   	else
   		render 'new'
   	end
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password,
+  																:password_confirmation)
   end
 end
