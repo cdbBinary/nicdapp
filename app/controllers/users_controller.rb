483148d727c53163d8fa873d8ce95c7282e31383
cdb.binary <cdb.binary@gmail.com>
2017-04-25 00:54:35 -0600

    add code base for testing on models on users edit and users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
 	def show
 		@user = User.find(params[:id])
 	end
@@ -24,16 +26,25 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attribute(user_params)
-      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
   end
 
   prvate
-  
+
     def user_params
     	params.require(:user).permit(:name, :email, :password,
     																:password_confirmation)
+    end
+
+    #Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
     end
 end
