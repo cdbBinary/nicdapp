6a38b0a9cf62c24bd3031e3210cb3994560a3f3d
cdb.binary <cdb.binary@gmail.com>
2017-05-07 16:11:16 -0600

    minor change for my work route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   # Static Page Paths
-  get  '/my work',    to: 'static_pages#my_work'
+  get  '/my work',    to: 'static_pages#mywork'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 
