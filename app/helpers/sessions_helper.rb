07bb39a2febf2a44cd3cc98fc22437b536157e38
cdb.binary <cdb.binary@gmail.com>
2017-05-07 15:47:05 -0600

    copied over nicd cleaning up

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,5 @@
 module SessionsHelper
-	#Logs in the given user.
+	# Logs in the given user.
 	def log_in(user)
 		session[:user_id] = user.id
 	end
@@ -11,17 +11,17 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-   # Returns true if the given user is the current user.
+  # Returns true if the given user is the current user.
   def current_user?(user)
     user == current_user
   end
 
-	#Returns the current logged-in user (if any).
+	# Returns the current logged-in user (if any).
 	def current_user
 		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
-	#Returns true if the user is logged in. False otherwise.
+	# Returns true if the user is logged in. False otherwise.
 	def logged_in?
 		!current_user.nil?
 	end
