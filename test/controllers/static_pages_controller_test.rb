6aae91dfaae1cde1fd65c21c6ed1d679b6197fd1
cdb.binary <cdb.binary@gmail.com>
2017-05-25 13:34:40 -0400

    first change fix tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,27 +2,31 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+def setup
+  @base_title = "Nailed It"
+end
+
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Nailed It"
+    assert_select "title", "Home | #{@base_title}"
   end
 
-  test "should get help" do
+  test "should get mywork" do
     get mywork_path
     assert_response :success
-    assert_select "title", "Nailed It"
+    assert_select "title", "My Work | #{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "Nailed It"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Nailed It"
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
